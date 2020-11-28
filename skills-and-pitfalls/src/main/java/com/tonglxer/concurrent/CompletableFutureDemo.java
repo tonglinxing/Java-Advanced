ef6bb683153ddcd34038fd16a6f094b5b1170818
tonglinxing <896872228@qq.com>
2020-11-29 00:34:39 +0800

    CompletableFuture回调函数 1.thenApply() 2.thenAccept() 3.thenRun()

--- skills-and-pitfalls/src/main/java/com/tonglxer/concurrent/CompletableFutureDemo.java
+++ skills-and-pitfalls/src/main/java/com/tonglxer/concurrent/CompletableFutureDemo.java
@@ -2,9 +2,7 @@ package com.tonglxer.concurrent;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  *
@@ -18,7 +16,8 @@ public class CompletableFutureDemo {
 
 
     public static void main(String[] args) {
-        instance.runAsyncDemo();
+//        instance.runAsyncDemo();
+        instance.supplyAsyncDemo();
     }
 
     public CompletableFutureDemo getInstance() {
@@ -29,17 +28,45 @@ public class CompletableFutureDemo {
     private void runAsyncDemo() {
         CompletableFuture<Void> completableFuture = CompletableFuture.runAsync(() -> {
             try {
-                Thread.sleep(1000);
+                Thread.sleep(5000);
             } catch (InterruptedException e) {
                 log.error("runAsyncDemo is interrupted.");
             }
-            System.out.println("hello");
         });
         try {
-            System.out.println("I am done." + completableFuture.get());
+            completableFuture.get(); // get()方法会阻塞当前进程直至任务完成
+            System.out.println("I am done.");
         } catch (InterruptedException | ExecutionException e) {
             log.error("runAsyncDemo is interrupted or execution error.");
         }
 
+    }
+
+    private void supplyAsyncDemo() {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                log.error("supplyAsyncDemo is interrupted.");
+            }
+            return "supplyAsyncDemo is done.";
+        }, executor).thenApply(r -> {
+            System.out.println("任务完成，正在执行Apply回调函数...");
+            return "Apply: " + r;
+        });
+        future.thenRun(() -> {
+            System.out.println("Apply回调执行结束，执行Run回调函数...");
+        });
+        future.thenAccept(r -> {
+            System.out.println("Run回调执行结束，执行Accept回调函数...");
+        });
+        System.out.println("任务已创建，进入主线程..");
+        try {
+            future.get();
+            executor.shutdown();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 }
