1a7f90b4e45db03f3840cc2ee61b0625bdf56d86
tonglinxing <896872228@qq.com>
2020-11-29 20:58:13 +0800

    clean code

--- skills-and-pitfalls/src/main/java/com/tonglxer/concurrent/FutureTaskDemo.java
+++ skills-and-pitfalls/src/main/java/com/tonglxer/concurrent/FutureTaskDemo.java
@@ -16,7 +16,28 @@ import java.util.concurrent.ExecutionException;
  */
 @Slf4j
 public class FutureTaskDemo {
+
+    /**
+     * <Question>为什么《阿里巴巴Java开发手册》上要禁止使用Executors来创建线程池?</Question>
+     *
+     * Executors创建出来的线程池使用的全都是无界队列
+     * 而使用无界队列会带来很多弊端，最重要的就是，它可以无限保存任务，因此很有可能造成OOM异常。
+     * 同时在某些类型的线程池里面
+     * 使用无界队列还会导致maximumPoolSize、keepAliveTime、handler等参数失效。
+     *
+     * */
+
+
     public static void main(String[] args) {
+        /**
+         * CPU密集型：线程个数为CPU核数。
+         * 这几个线程可以并行执行，不存在线程切换到开销
+         * 提高了cpu的利用率的同时也减少了切换线程导致的性能损耗
+         *
+         * IO密集型：线程个数为CPU核数的两倍。
+         * 其中的线程在IO操作的时候，其他线程可以继续用cpu，提高了cpu的利用率。
+         *
+         * */
         ThreadPoolExecutor threadPool = new ThreadPoolExecutor(
                 // 核心线程 -> 阻塞队列 -> 创建非核心线程 -> 达到最大线程数后执行拒绝策略
                 2, 5,
