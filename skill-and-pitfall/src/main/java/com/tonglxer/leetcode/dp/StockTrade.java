948d56ce90bb3df305de94b226de3d4c0f4e910d
tonglinxing <896872228@qq.com>
2020-12-03 22:53:48 +0800

    股票买卖问题： 1. n变化，k=1 2. 增加注释

--- skill-and-pitfall/src/main/java/com/tonglxer/leetcode/dp/StockTrade.java
+++ skill-and-pitfall/src/main/java/com/tonglxer/leetcode/dp/StockTrade.java
@@ -14,9 +14,11 @@ public class StockTrade {
 
     private static int maxProfit(int[] prices, int k) {
         int n = prices.length;
+        // 边界条件永远是最容易忽视的点
         if (n == 0 | k <= 0) {
             return 0;
         }
+        // 三维dp数组，天数、买卖次数、是否持有股票
         int[][][] dp = new int[n][k+1][2];
         for (int i=0; i<n; i++) {
             for (int j=k; j>=1; j--) {
@@ -34,6 +36,9 @@ public class StockTrade {
 
     private static int maxProfit_Two(int[] prices) {
         int n = prices.length;
+        if (n == 0) {
+            return 0;
+        }
         int dp_1_0 = 0;
         int dp_1_1 = Integer.MIN_VALUE;
         int dp_2_0 = 0;
@@ -42,8 +47,37 @@ public class StockTrade {
             dp_1_0 = Math.max(dp_1_0, dp_1_1 + prices[i]);
             dp_1_1 = Math.max(dp_1_1, -prices[i]);
             dp_2_0 = Math.max(dp_2_0, dp_2_1 + prices[i]);
-            dp_2_1 = Math.max(dp_2_1, dp_1_1 - prices[i]);
+            dp_2_1 = Math.max(dp_2_1, dp_1_0 - prices[i]);
         }
         return dp_2_0;
+    }
+
+    private static int maxProfit_One(int[] prices) {
+//        int n = prices.length;
+//        if (n == 0) {
+//            return 0;
+//        }
+//        int dp_0 = 0;
+//        int dp_1 = Integer.MIN_VALUE;
+//        for (int i=0; i<n; i++) {
+//            dp_0 = Math.max(dp_0, dp_1 + prices[i]);
+//            // 当只能交易一次时，那买入的那天的利润为 0-prices[i]
+//            dp_1 = Math.max(dp_1, -prices[i]);
+//        }
+//        return dp_0;
+
+        // 更高效的方法
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i=0; i< prices.length; i++) {
+            if (prices[i] < minPrice) {
+                // 维护最低价格
+                minPrice = prices[i];
+            } else if ((prices[i] - minPrice) > maxProfit) {
+                // 更新最大利润
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
     }
 }
