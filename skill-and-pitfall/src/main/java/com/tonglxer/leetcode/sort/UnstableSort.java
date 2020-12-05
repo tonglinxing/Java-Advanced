4aab327e8649b21f475dac0565ee854a9e520e5c
tonglinxing <896872228@qq.com>
2020-12-06 00:12:17 +0800

    不稳定排序： 1. 快速排序

--- skill-and-pitfall/src/main/java/com/tonglxer/leetcode/sort/UnstableSort.java
+++ skill-and-pitfall/src/main/java/com/tonglxer/leetcode/sort/UnstableSort.java
@@ -1,5 +1,8 @@
 package com.tonglxer.leetcode.sort;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * 不稳定排序
  * 快排、希尔、堆、简单选择
@@ -8,4 +11,39 @@ package com.tonglxer.leetcode.sort;
  * @date 2020/11/27
  */
 public class UnstableSort {
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {
+                4,5,2,6,1,9,0
+        };
+        quickSort(nums, 0, 6);
+        for (int n : nums) {
+            System.out.print(n + " ");
+        }
+        System.out.println();
+    }
+
+    private static void quickSort(int[] nums, int low, int high) {
+        int n = nums.length;
+        if (n == 0 || low >= high ) {
+            return;
+        }
+        int left = low;
+        int right = high;
+        int temp = nums[left];
+        while (left < right) {
+            while (left < right && nums[right] >= temp) {
+                right--;
+            }
+            while (left < right && nums[left] <= temp) {
+                left++;
+            }
+            nums[right] = nums[left];
+        }
+        nums[left] = temp;
+        System.out.println("Sorting: " + Arrays.toString(nums));
+        quickSort(nums, low, left-1);
+        quickSort(nums, left+1, high);
+
+    }
 }
