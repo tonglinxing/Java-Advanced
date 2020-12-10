d282ab539afb2dfe59fcd44e785d54c43523dfe8
tonglinxing <896872228@qq.com>
2020-12-10 22:55:56 +0800

    打家劫舍问题

---
+++ skill-and-pitfall/src/main/java/com/tonglxer/leetcode/dp/RobProblem.java
@@ -0,0 +1,28 @@
+package com.tonglxer.leetcode.dp;
+
+/**
+ * @Author Tong LinXing
+ * @date 2020/12/10
+ */
+public class RobProblem {
+
+    private static int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+//        int dp[] = new int[n+2];
+//        for (int i=n-1; i>=0; i--) {
+//            dp[i] = Math.max(dp[i+1], dp[i+2] + nums[i]);
+//        }
+//        return dp[0];
+        int dp_1 = 0, dp_2 = 0;
+        int ans = 0;
+        for (int i=n-1; i>=0; i--) {
+            ans = Math.max(dp_1, dp_2+nums[i]);
+            dp_2 = dp_1;
+            dp_1 = ans;
+        }
+        return ans;
+    }
+}
