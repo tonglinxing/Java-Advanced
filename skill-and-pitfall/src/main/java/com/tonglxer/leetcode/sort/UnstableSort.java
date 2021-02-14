be54f7cc5c1bfcaf8480a018a8e7a79d7c341a93
tonglinxing <896872228@qq.com>
2021-02-14 23:19:30 +0800

    clean code

--- skill-and-pitfall/src/main/java/com/tonglxer/leetcode/sort/UnstableSort.java
+++ skill-and-pitfall/src/main/java/com/tonglxer/leetcode/sort/UnstableSort.java
@@ -18,14 +18,17 @@ public class UnstableSort {
         quickSort(nums, 0, nums.length-1);
     }
 
-    private static void quickSort(int[] nums, int low, int high) {
-        int n = nums.length;
-        // 边界条件
-        if (n == 0 || low >= high ) {
+    private static void quickSort(int[] nums, int left, int right) {
+        if (nums == null || left >=right || nums.length<=1) {
             return;
         }
-        int left = low;
-        int right = high;
+        int mid = partition(nums, left, right);
+        // 开始递归
+        quickSort(nums, left, mid-1);
+        quickSort(nums, mid+1, right);
+    }
+
+    private static int partition(int[] nums, int left, int right) {
         // 最低位元素设置为哨兵
         int temp = nums[left];
         while (left < right) {
@@ -43,9 +46,8 @@ public class UnstableSort {
         nums[left] = temp;
         // 至此一次排序结束
         System.out.println("Sorting: " + Arrays.toString(nums));
-        // 开始递归
-        quickSort(nums, low, left-1);
-        quickSort(nums, left+1, high);
+        // 返回分界点随后继续递归
+        return left;
     }
 
 
